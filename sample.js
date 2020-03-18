1aa34e37be959fa52d9b4f96fb3076239e4fcffd
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-18 13:05:58 +0900

    編集完了

--- sample.js
+++ sample.js
@@ -1,23 +1,45 @@
 new Vue({
   // このelにHTMLのidを指定することで、そのidのDOMでVue.jsのインスタンスがインポートされる
-  el: '#app',
+  el: "#app",
   // dataは、定義されたVue.jsのインスタンスが持つ属性（値）
   data: {
-    name: 'A山B郎',
-    cource: 'Webエンジニアコース',
-    acceptancePeriod: '2019年01月期',
-    defaultLastId: 3,
+    newstudentText: "",
     students: [
-      { id: 1, name: '野呂浩良', cource: '機械学習コース', acceptancePeriod: '2019年01月期' },
-      { id: 2, name: '富永修司', cource: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
-      { id: 3, name: '斉藤一起', cource: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
-    ]
+      {
+        id: 1,
+        name: "野呂浩良",
+        cource: "機械学習コース",
+        acceptancePeriod: "2019年01月期"
+      },
+      {
+        id: 2,
+        name: "富永修司",
+        cource: "Webエンジニアコース",
+        acceptancePeriod: "2017年11月期"
+      },
+      {
+        id: 3,
+        name: "斉藤一起",
+        cource: "Webエンジニアコース",
+        acceptancePeriod: "2017年11月期"
+      }
+    ],
+    nextstudentId: 4
   },
   // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
   methods: {
     addStudent: function() {
-        // この function() { } の中にaddStudentメソッド
-        // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
+      this.students.push({
+        id: this.nextstudentId++,
+        name: this.newstudentText,
+        cource: this.cource,
+        acceptancePeriod: this.acceptancePeriod
+      });
+      this.newstudentText = "";
+      this.cource = "";
+      this.acceptancePeriod = "";
+      // この function() { } の中にaddStudentメソッド
+      // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
     }
   }
-})
+});
